ec343f9f1bf8d88eb170057f10e782f4cfc105c7
Arunima Mandwariya <Arunima@Arunimas-MacBook-Air.local>
2026-05-02 12:17:02 +0530

    fix: remove duplicate default export in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
 export default defineConfig({
-  base: '/YOUR_REPOSITORY_NAME/', // Use the name you gave your repo on GitHub
   plugins: [react()],
+  base: '/vinyl-repository/', 
 })
